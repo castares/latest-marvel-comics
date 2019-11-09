250680443407c81c267468d35430bf68ca75a26c
castares <cesar.castares@gmail.com>
2019-11-09 14:00:23 +0100

    connected to API

--- src/api_requests.py
+++ src/api_requests.py
@@ -1,18 +1,41 @@
 import os
 import requests
+import hashlib
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+#keys
+public_key = os.getenv("MARVEL_API_PUBLIC_KEY")
+private_key = os.getenv("MARVEL_API_PRIVATE_KEY")
+
+print(public_key, private_key)
+
+#ts
+ts = str(time.time())
+
+def hash(ts, public_key, private_key):
+    m_hash = hashlib.md5()
+    ts_str_byte = bytes(ts, 'utf-8')
+    private_key_byte = bytes(private_key, 'utf-8')
+    public_key_byte = bytes(public_key, 'utf-8')
+    m_hash.update(ts_str_byte + private_key_byte + public_key_byte)
+    return str(m_hash.hexdigest())
 
 def MarvelRequest(resource):
-    authToken = os.getenv("MARVEL_API_TOKEN")
-    if not authToken:
-        raise ValueError("NECESITAS UN TOKEN")
-    else:
-        print("We have a MARVEL token: ", authToken[0:4])
-    headers = {
-        "Authorization": "token {}".format(authToken)
+    print(ts)
+    print(public_key)
+    url = "http://gateway.marvel.com/v1/public{}".format(resource)
+    payload = {
+        'ts': ts, 
+        'apikey': public_key, 
+        'hash': hash(ts, public_key, private_key)
     }
-    url = "http://gateway.marvel.com/v1/public/comics{}".format(resource)
-    print("Requesting authorized {}".format(url))
-    res = requests.get(url, headers=headers)
-    return res
+    r = requests.get(url, params=payload)
+    print(r.url)
+    return r
 
-MarvelRequest("?name=Spider-Man")
+answer = MarvelRequest("/characters?name=Spider-Man").json()
+
+print(answer)
