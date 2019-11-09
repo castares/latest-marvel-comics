71278183403730b73b5867c489552bb63a1271d5
castares <cesar.castares@gmail.com>
2019-11-09 18:47:22 +0100

    working on it

--- src/web_scrapping.py
+++ src/web_scrapping.py
@@ -4,17 +4,29 @@ def cooksoup(url, parser='html.parser'):
     html = requests.get(url).text
     return BeautifulSoup(html, parser)
 
-def scrap_to_df(items):
+def scrap_to_df(search):
     #Given a BeautifulSoup object and an iterable, 
     #returns a DataFrame without blankspaces and applyingline-breaks.
     lst= []
-    for e in items:
+    for e in search:
         lst.append(e.text.strip().split('\n'))
     return pd.DataFrame(lst)
 
 def main():
+    #Cook the soup.
     soup = cooksoup('https://previewsworld.com/Catalog?batch=NOV19&pub=MARVEL%20COMICS')
-    return scrap_to_df(soup.find_all("div", {"class": "nrgIC"}))
+
+    #Build a dataframe with previews from Marvel.
+    data = scrap_to_df(soup.find_all("div", {"class": "nrgIC"}))
+    data.drop([2], axis=1, inplace=True)
+    data.set_axis(['Reference','Publisher','Title','Price'], axis=1, inplace=True)
+    marvel_previews = data[data['Publisher'] == "MARVEL COMICS"]
+    
+    #Retrieve orders due date:
+    due_disclaimer = soup.find_all("div", {"class" : "ordersDueDisclaimer"})
+    due_date = due_disclaimer[0].text.strip()
+    print(due_date)
+    return marvel_previews.to_csv("../output/marvel_previews.csv")
 
 if __name__=="__main__":
     
